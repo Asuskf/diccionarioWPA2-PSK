ee76b19f6bfbe2b7b5697a4c422017f2724c17d9
Asuskf <36687747+Asuskf@users.noreply.github.com>
2018-06-01 23:50:21 -0500

    Update DiccionarioNetlife.py

--- DiccionarioNetlife.py
+++ DiccionarioNetlife.py
@@ -1,3 +1,4 @@
+from stdnum.ec import ci, ruc
 import sys
 # _*_coding:utf-8_*_
 class crearDiccionario:
@@ -8,58 +9,14 @@ class crearDiccionario:
         inicio = int(self.inicioCedula)
         final = int(self.finalCedula)
         archivo = open(self.provincia+'DiccionarioWPA.txt','w')
-        if inicioCedula[:1] == '0':
-            while inicio <= final:
-                total = 0
-                numeroLetras = str(inicio)
-                tipo = int(numeroLetras[1])
-                if tipo >= 0 and tipo <= 6:
-                    base = 10
-                    digitoVerificador = int(numeroLetras[-1])
-                    proceso = (1, 2, 1, 2, 1, 2, 1, 2)
-                elif (tipo == 6):
-                    base = 11
-                    digitoVerificador = int(numeroLetras[-1])
-                    proceso = (3, 2, 7, 6, 5, 4, 3, 3)
-                for i in range(0, len(proceso)):
-                    calcular = int(numeroLetras[i]) * proceso[i]
-                    if (tipo >= 0 and tipo <= 6) or tipo == 9:
-                        total += calcular if calcular < 10 else int(str(calcular)[0]) + int(str(calcular)[1])
-                    else:
-                        total += calcular
-                modulo = total % base
-                val = base - modulo if modulo != 0 else 0
-                validar = val == digitoVerificador
-                if validar == True:
-                    archivo.write('0'+numeroLetras + '\n')
-                    archivo.write('0'+numeroLetras + '001\n')
-                inicio += 1
-        else:
-            while inicio <= final:
-                total = 0
-                numeroLetras = str(inicio)
-                tipo = int(numeroLetras[2])
-                if tipo >= 0 and tipo <= 6:
-                    base = 10
-                    digitoVerificador = int(numeroLetras[-1])
-                    proceso = (2, 1, 2, 1, 2, 1, 2, 1, 2)
-                elif (tipo == 6):
-                    base = 11
-                    digitoVerificador = int(numeroLetras[-1])
-                    proceso = (4, 3, 2, 7, 6, 5, 4, 3, 3)
-                for i in range(0, len(proceso)):
-                    calcular = int(numeroLetras[i]) * proceso[i]
-                    if (tipo >= 0 and tipo <= 6) or tipo == 9:
-                        total += calcular if calcular < 10 else int(str(calcular)[0]) + int(str(calcular)[1])
-                    else:
-                        total += calcular
-                modulo = total % base
-                val = base - modulo if modulo != 0 else 0
-                validar = val == digitoVerificador
-                if validar == True:
-                    archivo.write(numeroLetras + '\n')
-                    archivo.write(numeroLetras + '001\n')
-                inicio += 1
+        while inicio <= final:
+            numeroLetras = str(inicio)
+            validarRuc = numeroLetras + '001'
+            if ci.is_valid(numeroLetras):
+                archivo.write(numeroLetras + '\n')
+            if ruc.is_valid(validarRuc):
+                archivo.write(validarRuc+'\n')
+            inicio += 1
         archivo.close()
         print('Diccionario creado correctamente suerte en tú ataque')
 
